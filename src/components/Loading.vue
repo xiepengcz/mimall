52a4113966a4c004283b30cfe954ed16c63df4a4
谢芃 <949026370@qq.com>
2020-04-14 18:20:07 +0800

    '完成订单页面'

---
+++ src/components/Loading.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="loading">
+    <img src="/imgs/loading-svg/loading-spinning-bubbles.svg" alt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loading'
+}
+</script>
+
+<style lang="scss">
+.loading {
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+  margin: 40px auto;
+  img {
+    height: 100px;
+    width: 100%;
+  }
+}
+</style>
--- src/pages/aliPay.vue
+++ src/pages/aliPay.vue
@@ -1,12 +1,51 @@
 <template>
-  <div>
-    alipay
+  <div class="alipay">
+    <order-header title="订单支付">
+      <template v-slot:tip>
+        <span>请谨防钓鱼链接或诈骗电话，了解更多></span>
+      </template>
+    </order-header>
+    <loading v-if="loading"></loading>
+    <div class="from" v-html="content"></div>
   </div>
 </template>
 
 <script>
+import OrderHeader from '../components/OrderHeader'
+import Loading from '../components/Loading'
 export default {
-  name: 'alipay'
+  name: 'alipay',
+  data() {
+    return {
+      orderId: this.$route.query.orderId,
+      content: '',
+      loading: true
+    }
+  },
+  components: {
+    OrderHeader,
+    Loading
+  },
+  mounted() {
+    this.paySubmit()
+  },
+  methods: {
+    paySubmit() {
+      this.axios
+        .post('/pay', {
+          orderId: this.orderId,
+          orderName: 'Vue高仿小米商城',
+          amount: 0.01,
+          payType: 1
+        })
+        .then(res => {
+          this.content = res.content
+          setTimeout(() => {
+            document.forms[0].submit()
+          }, 1000)
+        })
+    }
+  }
 }
 </script>
 
